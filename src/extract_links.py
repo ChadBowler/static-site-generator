caada04541fe55c8ba074c55a57568f723dfad30
ChadBowler <cbowlerdev@gmail.com>
2024-06-09 10:47:43 -0600

    added functions to split image and link nodes

--- src/extract_links.py
+++ src/extract_links.py
@@ -1,4 +1,14 @@
 import re
+from textnode import (
+    TextNode,
+    TEXT,
+    BOLD,
+    ITALIC,
+    CODE,
+    LINK,
+    IMAGE,
+    delimiters
+)
 
 def extract_markdown_images(text):
     reg_pattern = r"!\[(.*?)\]\((.*?)\)"
@@ -9,3 +19,62 @@ def extract_markdown_links(text):
     reg_pattern = r"\[(.*?)\]\((.*?)\)"
     images = re.findall(reg_pattern, text)
     return images
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        images = extract_markdown_images(old_node.text)
+        if not images:
+            new_nodes.append(old_node)
+        else:
+            for image in images:
+                sections = old_node.text.split(f"![{image[0]}]({image[1]})", 1)
+                if len(sections) != 2:
+                    raise ValueError("Invalid markdown, image section not closed")
+                split_nodes.append(TextNode(sections[0], TEXT))
+                split_nodes.append(TextNode(image[0], IMAGE, image[1]))
+                old_node.text = sections[-1]
+            if old_node.text:
+                split_nodes.append(TextNode(old_node.text, TEXT))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        links = extract_markdown_links(old_node.text)
+        if not links:
+            new_nodes.append(old_node)
+        else:
+            for link in links:
+                sections = old_node.text.split(f"[{link[0]}]({link[1]})", 1)
+                split_nodes.append(TextNode(sections[0], TEXT))
+                split_nodes.append(TextNode(link[0], LINK, link[1]))
+                old_node.text = sections[-1]
+            if old_node.text:
+                split_nodes.append(TextNode(old_node.text, TEXT))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+
+# node = TextNode(
+#     "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+#     TEXT,
+# )
+# new_nodes = split_nodes_image([node])
+# print(new_nodes)
+
+# node = TextNode(
+#     "This is text with a [link](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another [second link](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png) with some extra text at the end.",
+#     TEXT,
+# )
+# new_nodes = split_nodes_link([node])
+# print(new_nodes)
--- src/test_extract_links.py
+++ src/test_extract_links.py
@@ -1,6 +1,14 @@
 import unittest
 
-from extract_links import extract_markdown_images, extract_markdown_links
+from extract_links import (
+    extract_markdown_images, 
+    extract_markdown_links, 
+    split_nodes_image,
+    split_nodes_link,
+    TextNode, 
+    TEXT,
+    IMAGE,
+    LINK)
 
 class TestTextNode(unittest.TestCase):
     def test_images(self):
@@ -15,6 +23,38 @@ class TestTextNode(unittest.TestCase):
         output = [('link', 'https://www.example.com'), ('another', 'https://www.example.com/another')]
         self.assertEqual(extracted, output)
 
+    def test_node_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+            TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        output = [
+                TextNode("This is text with an ", TEXT),
+                TextNode("image", IMAGE, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+                TextNode(" and another ", TEXT),
+                TextNode(
+                    "second image", IMAGE, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png"
+                    ),
+                ]
+        self.assertEqual(new_nodes, output)
+
+    def test_node_split_link(self):
+        node = TextNode(
+            "This is text with a [link](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another [second link](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png) with some extra text at the end.",
+            TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        output = [
+                TextNode("This is text with a ", TEXT),
+                TextNode("link", LINK, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+                TextNode(" and another ", TEXT),
+                TextNode(
+                    "second link", LINK, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png"
+                    ),
+                TextNode(" with some extra text at the end.", TEXT)
+                ]
+        self.assertEqual(new_nodes, output)
 
 if __name__ == "__main__":
     unittest.main()
