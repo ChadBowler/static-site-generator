b95a4ce51c11bf898abca9c37937915fb3ee0e04
ChadBowler <cbowlerdev@gmail.com>
2024-06-10 19:47:12 -0600

    wrote recursive function to generate pages.

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,5 +1,10 @@
 import os
+import re
+from pathlib import Path
 from split_blocks import markdown_to_html
+
+md_pattern = re.compile(r"[a-zA-Z0-9]+\.md")
+file_pattern = re.compile(r"[a-zA-Z0-9]+\.[a-z]{,4}")
 
 def extract_title(markdown):
     lines = markdown.split("\n")
@@ -22,3 +27,24 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, 'w', encoding="utf-8") as f:
         f.write(temp_contents)
  
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    content_dir_files = os.listdir(dir_path_content)
+    files = []
+    directories = []
+    for file in content_dir_files:
+        if re.match(file_pattern, file):
+            files.append(file)
+        else:
+            directories.append(file)
+    for file in files:
+        if re.match(file_pattern, file):
+            if re.match(md_pattern, file):
+                new_dir_name = dir_path_content.split("/")[-1]
+                os.mkdir(os.path.join(dest_dir_path, new_dir_name))
+                if new_dir_name != "content":
+                    new_dir = os.path.join(dest_dir_path, new_dir_name)
+                else:
+                    new_dir = dest_dir_path
+                generate_page(os.path.join(dir_path_content, file), template_path, os.path.join(new_dir, "index.html"))
+    for folder in directories:
+        return generate_pages_recursive(os.path.join(dir_path_content, folder), template_path, dest_dir_path)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from copy_static import clear_public
-from generate_page import generate_page
+from generate_page import generate_pages_recursive
 
-from_path = "./content/index.md"
+dir_path_content = "./content"
 template_path = "./template.html"
-dest_path = "./public/index.html"
+dest_dir_path = "./public"
 
 def main():
     clear_public()
-    generate_page(from_path, template_path, dest_path)
+    generate_pages_recursive(dir_path_content, template_path, dest_dir_path)
 
 if __name__ == "__main__":
     main()
